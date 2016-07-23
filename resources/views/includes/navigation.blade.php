7310c5932bc0287011e9c6bd6e767915795d0a46
Bryan Reed <test@test.com>
2016-07-22 23:31:58 -0700

    Fixed active state when on search and doc view pages

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -4,7 +4,7 @@
       	<?php
       	$requestName = Request::route()->getName();
       	?>
-        <li @if($requestName == 'home') class="active" @endif ><a href="{{route('home')}}">Home <span class="sr-only">(current)</span></a></li>
+        <li @if($requestName == 'home' || $requestName == 'doctor.search'|| $requestName == 'doctor.view') class="active" @endif ><a href="{{route('home')}}">Home <span class="sr-only">(current)</span></a></li>
         <li @if($requestName == 'account') class="active" @endif><a href="{{route('account')}}">My Account</a></li>
         <li @if($requestName == 'doctors') class="active" @endif><a href="{{route('doctors')}}">My Doctors</a></li>
         <li @if($requestName == 'doctor.manage') class="active" @endif><a href="{{route('doctor.manage')}}">Manage Doctor</a></li>
